7830111f5fe565d13d52965e744da7e45540f096
Gustavo Maia <141832829+gustavomaiaa@users.noreply.github.com>
2025-05-23 12:13:45 -0300

    Create vet_clinic.h

---
+++ vet_clinic.h
@@ -0,0 +1,12 @@
+#ifndef VET_CLINIC_H
+#define VET_CLINIC_H
+
+void* dog(void* arg);
+void* cat(void* arg);
+
+void dogWantsToEnter();
+void dogWantsToLeave();
+void catWantsToEnter();
+void catWantsToLeave();
+
+#endif
